b35bef091a2ed170b0969ea664a087a430305c7d
Dvir Farkash <dviref09@gmail.com>
2021-07-07 15:42:58 +0300

    commit

--- a.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-
-app = tkinter.Tk()
-app.title('hello')
-app.geometry("300x300+650+200")
-
-lbl = tkinter.Label(app, text="לא לגעת", font="144").pack(pady=75)
-
-app.mainloop()
--- run.py
+++ run.py
@@ -1,25 +1,3 @@
-from tkinter.constants import END
 import bin_interpreter as bin
-import tkinter
 
-app = tkinter.Tk()
-app.title('Mode select')
-
-app.geometry("500x500+650+200")
-def bin_to_number():
-    lbl = tkinter.Label(app, text ="Please enter binary code").pack(pady=25)
-    enter = tkinter.Entry(app).pack(pady=0)
-    button2 = tkinter.Button(app, text="Convert", command=run).pack()
-    return enter
-
-def run():
-    enter = bin_to_number()
-    enterget = enter.get()
-
-lbl = tkinter.Label(app, text="Please select mode:").pack(pady=25)
-
-button1 = tkinter.Button(app, text="Binary to number", command=bin_to_number).pack()
-
-lbl2 = tkinter.Label(app).pack()
-
-app.mainloop()
+bin.Run_bin()
